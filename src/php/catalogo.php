a4a909d85f941f1d6401bdae4653e8e6d5bd89e3
rcmarialice <maria.carneiro4@estudante.ifto.edu.br>
2024-09-16 20:17:19 -0300

    Atualização de codigos

---
+++ src/php/catalogo.php
@@ -0,0 +1,35 @@
+<h1>
+    Listagem de usuários
+</h1>
+<?php
+$con = new mysqli ("localhost","root", "", "auralux");
+$res = $con-> query("select * from auralux.categorias");
+echo"<table border =1 >";
+echo"<thead>";
+echo"<tr>";
+echo"<th> id </th>";
+echo"<th> nome </th>";
+echo"<th> preco </th>";
+
+echo"</tr>";
+echo"<thead>";
+echo"<tbody>";
+while($obj= $res -> fetch_object()){
+    $id= $obj->id;
+    echo "<tr>";
+    echo"<td>";
+    echo $obj->id;
+    echo"</td>";
+    echo"<td>";
+    echo $obj->nome;
+    echo"</td>";
+    echo"<td>";
+    echo $obj->preco;
+    echo"</td>";
+    echo"<td>";
+    echo"<tr>";
+}
+echo"</tbody>";
+echo "</table>";
+$con ->close();
+?>
