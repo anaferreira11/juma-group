179d041dab463da1908fde921ce00971b3130d10
anaferreira11 <ana.ferreira11@estudante.ifto.edu.br>
2024-09-19 15:04:10 -0300

    foi

---
+++ src/php/inserir.php
@@ -0,0 +1,16 @@
+<h1>
+    Cadastrar usuarios
+
+</h1>
+
+<?php
+include("conexao.php");
+$nome = $_POST["nome"];
+$senha = $_POST["senha"];
+$tel = $_POST["telefone"];
+$con-> query("insert into clientes (nome,senha,telefone) values ('$nome','$senha','$tel')");
+    
+
+
+$con ->close();
+?>
--- src/php/listarusuario.php
+++ src/php/listarusuario.php
@@ -1,15 +1,16 @@
 <h1>
-    Listagem de Produtos
+    Listagem de usuarios
 </h1>
 <?php
 include("conexao.php");
-$res = $con-> query("select * from auralux.categorias");
+$res = $con-> query("select * from auralux.clientes");
 echo"<table border =1 >";
 echo"<thead>";
 echo"<tr>";
 echo"<th> id</th>";
 echo"<th> nome </th>";
-echo"<th> Preco </th>";
+echo"<th> senha </th>";
+echo"<th> telefone </th>";
 echo"<th colspan='2'> Opções </th>";
 echo"</tr>";
 echo"<thead>";
@@ -24,10 +25,13 @@ while($obj= $res -> fetch_object()){
     echo $obj->nome;
     echo"</td>";
     echo"<td>";
-    echo $obj->preco;
+    echo $obj->senha;
     echo"</td>";
     echo"<td>";
-    echo "<a href='excluir.php?id=$id'> Excluir </a>";
+    echo $obj->telefone;
+    echo"</td>";
+    echo"<td>";
+    echo "<a href='excluirusuario.php?id=$id'> Excluir </a>";
     echo"</td>";
     echo"<td>";
     echo "<a href='editar.php?id=$id'> Editar </a>";
