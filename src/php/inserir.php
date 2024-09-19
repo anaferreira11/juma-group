1e8880f0ad770825c963fdac59ec19d539640d91
rcmarialice <maria.carneiro4@estudante.ifto.edu.br>
2024-09-19 16:15:02 -0300

    Atualização

--- src/php/inserir.php
+++ src/php/inserir.php
@@ -1,21 +1,4 @@
-<<<<<<< HEAD
-<?php
 
-include("conexao.php");
-$nome = $_POST["nome"];
-$preco= $_POST["preco"];
-$con-> query("insert into categorias (nome, preco ) values ('$nome', '$preco')");
-
-
-$con-> close();
-?>
-<br>
-
-<h1>
-    Parabéns, o Produto foi inserido!
-</h1>
-    <a href="catalogo.php">Ver usuários cadastrados</a>
-=======
 <h1>
     Cadastrar usuarios
 
@@ -32,4 +15,3 @@ $con-> query("insert into clientes (nome,senha,telefone) values ('$nome','$senha
 
 $con ->close();
 ?>
->>>>>>> db5de601503669bd8ad6fe3004815ffad17a00fe
