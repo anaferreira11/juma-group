aa1957d565ce5e61ff8c29c2c180686a3a280389
rcmarialice <maria.carneiro4@estudante.ifto.edu.br>
2024-09-25 10:04:38 -0300

    Css listar

--- src/php/categorias_produto/editar_categoria.php
+++ src/php/categorias_produto/editar_categoria.php
@@ -17,7 +17,7 @@ $con -> close();
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Auralux - Inserir Categoria</title>
+    <title>Auralux - Editar Categoria</title>
     <link rel="stylesheet" href="../../css/categoria.css">
 
 
@@ -26,7 +26,7 @@ $con -> close();
 </head>
 <body>
     <div class="container">
-        <h1>Inserir Categoria</h1>
+        <h1>Editar Categoria</h1>
         <form action="alterar-produto.php" method="post">
             <div class="form-group">
             <label >
--- src/php/categorias_produto/listarcategorias.php
+++ src/php/categorias_produto/listarcategorias.php
@@ -1,47 +1,60 @@
+<?php      
+  
+    $con = new mysqli("localhost", "root", "", "auralux");
+
+    if ($con->connect_error) {
+        die("Falha na conexão: " . $con->connect_error);
+    }
+
+ 
+    $res = $con->query("SELECT * FROM auralux.categorias");
+
+    
+    $categorias = [];
+    while ($obj = $res->fetch_object()) {
+        $categorias[] = $obj;
+    }
+
+    
+    $con->close();
+?>
+
 <!DOCTYPE html>
 <html lang="pt-BR">
 <head>
     <meta charset="UTF-8">
-
-    <title>Auralux - Categoria do Produto listagem: </title>
-    <link rel="stylesheet" href="../../css/categoria.css">
-
-
-
-
+    <title>Auralux - Categoria do Produto Listagem</title>
+    <link rel="stylesheet" href="../../css/listagem.css">
 </head>  
 <body>
-    <table border="1px">
-        <thead>
-            <tr>
-                <th>nome</th>
-            </tr>
-        </thead>
-        <tbody>
-            <?php       
-                $con = new mysqli ("localhost","root", "", "auralux");
-                $res = $con-> query("select * from auralux.categorias");
-                while($obj= $res -> fetch_object()){
-                    $id= $obj->id;
-                    echo "<tr>";
-                    echo"<td>";
-                    echo $obj->nome;
-                    echo"</td>";
-                    echo"<td>";
-                    echo "<a href='excluir_categoria.php?id=$id'> Excluir </a>";
-                    echo"</td>";
-                    echo"<td>";
-                    echo "<a href='editar_categoria.php?id=$id'> Editar </a>";
-                    echo"</td>";
-                
-                
-                    
-                    echo"</tr>";
-                }
-                $con ->close();
-            ?>
-        </tbody>
-    </table>
+
+    <div class="container">
+        <h1 class="options-header">Listagem de Categorias</h1>
+       
+        <table>
+            <thead>
+                <tr>
+                    <th>Nome</th>
+                    <th>Ações</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+                    // Exibindo os resultados
+                    foreach ($categorias as $obj) {
+                        $id = $obj->id;
+                        echo "<tr>";
+                        echo "<td>" . $obj->nome . "</td>";
+                        echo "<td>
+                                <a href='excluir_categoria.php?id=$id'>Excluir</a> |
+                                <a href='editar_categoria.php?id=$id'>Editar</a>
+                              </td>";
+                        echo "</tr>";
+                    }
+                ?>
+            </tbody>
+        </table>
+    </div>
+
 </body>
-
-
+</html>
