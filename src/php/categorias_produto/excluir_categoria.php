8a1270dc0616bd65bb7e653da41aee6b171c0e16
anaferreira11 <ana.ferreira11@estudante.ifto.edu.br>
2024-09-25 10:51:19 -0300

    Categoria Exluída

--- src/php/categorias_produto/excluir_categoria.php
+++ src/php/categorias_produto/excluir_categoria.php
@@ -3,9 +3,7 @@ $id = $_GET["id"];
 $con = new mysqli ("localhost","root", "", "auralux");
 $res = $con-> query("delete  from auralux.categorias where id = $id");
 
-
 $con -> close();
-
 ?>
 <br>
 
@@ -13,11 +11,22 @@ $con -> close();
 <html lang="pt-BR">
 <head>
     <meta charset="UTF-8">
-
     <title>Auralux - Categoria do Produto listagem: </title>
-    <link rel="stylesheet" href="../css/categoria.css">
+    <link rel="stylesheet" href="../../css/excluir_categoria.css">
 </head>
 <body>
-    <h1>categoria excluída!</h1>
-    <a href="../categorias_produto/listarcategorias.php">conferir</a>
+    <div class="container">
+        <h1>Categoria excluida!</h1>
+            <div class="form-group">
+            <a href="listarcategorias.php">
+                <button type="button">Listar Novamente</button>
+            </a>            </div>
+       </form>
+    
+      
+
+
+    </div>
+    
 </body>
+</html>
