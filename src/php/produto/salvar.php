ef2efce238b80bd5f1c58f2589b400792b93e4d0
ANA JULIA DA SILVA FERREIRA <20221120690149@ifto.local>
2024-11-13 12:27:54 -0300

    alterações 12:27

--- src/php/produto/salvar.php
+++ src/php/produto/salvar.php
@@ -7,8 +7,9 @@ $con = new mysqli ("localhost","root","", "auralux");
 $con->query("insert into produtos(nome,preco) values ('$nome','$preco')");
 
 $con ->close();
-?> 
+?>
 
 <h1> Configurar Produtos<h1>
 <a href="listagem.php">
 <button type="button">OK</button>
+<link rel="stylesheet" href="../../../html/salvarproduto.html">
