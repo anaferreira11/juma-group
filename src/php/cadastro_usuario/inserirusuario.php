5e631870b346147c95afc93ca53512ebdbe6c689
rcmarialice <maria.carneiro4@estudante.ifto.edu.br>
2024-11-14 13:41:31 -0300

    Css crude

--- src/php/cadastro_usuario/inserirusuario.php
+++ src/php/cadastro_usuario/inserirusuario.php
@@ -7,6 +7,28 @@ $con->query("insert into clientes(nome,email,senha) values ('$nome','$email','$s
 $con->close();
 
 ?>
+<br>
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Auralux -Usuario Cadastrado</title>
+    <link rel="stylesheet" href="../../css/excluir_categoria.css">
 
-<h1>ok</h1>
-<a href="listarusuario.php">Listagem de usuário</a>
+</head>
+    
+<body>
+    <div class="container">
+        <h1>Usuario Cadastrado!</h1>
+            <div class="form-group">
+            </a>            </div>
+            <a href="listarusuario.php">Listagem de usuário</a>
+       </form>
+
+
+    </div>
+</body>
+</html>
+
+
